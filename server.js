24d4c2a19aa96dbe7c87a137f3056b62943746ac
Chris Phillips <cp.phillips15@gmail.com>
2021-12-19 18:57:37 -0500

    added function to read db, then called to function in api get request

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.static('public'));
 
 // TODO: Write DB script
     // Read file to access db.json
+    readDb = () => {
+        return readFileAsync('./dc/db.json', 'utf8').then(data => JSON.parse(data))
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
 
     // Write to file to update db.json
 
@@ -22,11 +28,18 @@ app.use(express.static('public'));
 // TODO: get requests to api/notes for notes.html
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
+app.get('/api/notes', async (req, res) => {
+    // get data from db, then set it equal to notesArr (async?)
+    const dbData = await readDb();
+    const notesArr = dbData
+    res.json(notesArr)
+});
+
 // TODO: post request for api/notes
 
 
 
-// TODO: get * should read db.json
+//get * to load initial html
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
 
