dae111f9abc2ea9ddfcd175fd1423f02f713f8e7
Chris Phillips <cp.phillips15@gmail.com>
2021-12-19 18:33:10 -0500

    added TODOs based on assignment's Getting Started section

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+// TODO: Write DB script
+    // Read file to access db.json
+
+    // Write to file to update db.json
+
+    // Translate db data to notes
+
+
+// TODO: get requests to api/notes for notes.html
+
+// TODO: post request for api/notes
+
+// TODO: get * should read db.json
+
+
+
+// TODO: BONUS add delete fucntion
+
 app.get("/", function(req,res){
     res.send("boilerplate");
 });
