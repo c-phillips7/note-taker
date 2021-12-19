1d7151371dc8a7353a489b5cd5c87b445bf978bb
Chris Phillips <cp.phillips15@gmail.com>
2021-12-19 18:42:22 -0500

    added express middleware predicted I will need and static public to load all those files

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 // TODO: Write DB script
     // Read file to access db.json
