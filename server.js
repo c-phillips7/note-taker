8353d4e0ce1b94863299e1bd374c2f42f64373e7
Chris Phillips <cp.phillips15@gmail.com>
2021-12-19 18:25:56 -0500

    used PORT variable to access server from diff boilerplate found online

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const fs = requre('fs');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 app.get("/", function(req,res){
     res.send("boilerplate");
 });
 
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(PORT, function(){
+    console.log("Server started on PORT" + PORT);
 });
