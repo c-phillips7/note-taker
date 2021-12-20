c1aa0fa0b1b6d3f92af08cfcff292105736003f3
Chris Phillips <cp.phillips15@gmail.com>
2021-12-19 19:41:40 -0500

    added function to post request to api/notes, then wrote function to WritetoFileto save new post to db.json

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Write DB script
+// TODO: Write DB scripts
     // Read file to access db.json
     readDb = () => {
         return readFileAsync("./db/db.json", "utf8").then(data => JSON.parse(data))
@@ -23,18 +23,23 @@ const writeFileAsync = util.promisify(fs.writeFile);
     }
 
     // Write to file to update db.json
-    writeToFile = notes => {
-        
-    }
+    writetoFile = notes => {
+        writeFileAsync("./db/db.json", JSON.stringify(notes))
+            .then(() => console.log("wrote to db.json"))
+            .catch(function (err) {
+                console.log(err);
+            });
+    };
 
-    // Translate db data to notes?
+// TODO: Add function to add id to array so html will display note on click 
+        // starts with 1, not 0 like array
 
 
 
 // get requests to api/notes for notes.html
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
-app.get('/api/notes', async (req, res) => {
+app.get("/api/notes", async (req, res) => {
     // get data from db, then set it equal to notesArr (async?)
     const dbData = await readDb();
     const notesArr = dbData
@@ -42,16 +47,28 @@ app.get('/api/notes', async (req, res) => {
 });
 
 // TODO: post request for api/notes
+app.post("/api/notes", async (req, res) => {
+    const dbData = await readDb();
+    const notesArr = dbData;
+    const newNote = req.body;
+
+    // Before push, need to add "id" to json
+    notesArr.push(newNote);
+    writetoFile(notesArr);
+    return res.json(newNote);
+
+});
 
 
 
-//get * to load initial html
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
 
 
 // TODO: BONUS add delete fucntion
 
+
+//get * to load initial html
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
 
 app.listen(PORT, function(){
